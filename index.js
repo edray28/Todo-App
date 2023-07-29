f813c81d78a2035441a18bad7de815d6a6ba9a53
edray28 <edraytuyor@gmail.com>
2023-07-29 17:39:34 +0800

    Rename AppJs > Index, added dotenv, change mongodb URI, fixed port variable

---
+++ index.js
@@ -0,0 +1,44 @@
+//modules
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+
+
+mongoose.set('strictQuery', false);
+const dbcon = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }, console.log(`Database Connected ${process.env.MONGO_URI}`));
+    } catch (error) {
+        console.log("Database Connection Failed: " + error);
+        process.exit(1);
+    };
+};
+
+
+//middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.set("view engine", "pug");
+
+//routes 
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
+
+//server 
+dbcon().then(() => {
+    app.listen(3000, () => {
+        console.log(`Server Started and Port running at ${PORT}`);
+    })
+})
+
+
+
+
+
